90cefc3cd8e291e4604480fb755c2c0ed2b1c4f8
FelipeCorerato <fcorerato@gmail.com>
2019-11-09 15:15:52 -0200

    Updates at main
    :bank:

--- Programa/main.c
+++ Programa/main.c
@@ -397,56 +397,67 @@ void DecompressFile(const char *arquivoEntrada, const char *arquivoSaida)
     fclose(entrada);
 }
 
+void instrucoes()
+{
+    printf("Uso: main [OPCAO] [ARQUIVO] [ARQUIVO]\n\n");
+    printf("Opcoes:\n");
+    printf("\t-c\t\tComprime\n");
+    printf("\tExemplo: \t./main -c comprima.isso nisso.pd\n");
+    printf("\n\t-x\t\tDescomprime\n");
+    printf("\tExemplo: \t./main -x descomprima-isso.pd nisso.extensao\n");
+    printf("\n\t-h, --help\tInformacoes de ajuda\n");
+}
+
 int main(int argc, char *argv[])
 {
     // Caso os parâmetros informados sejam insuficientes
-    if (argc < 4)
+    if (argc < 4 && strcmp("-h", argv[1]) != 0 && strcmp("--help", argv[1]) != 0)
     {
-        printf("Uso: main [OPCAO] [ARQUIVO] [ARQUIVO]\n\n");
-        printf("Opcoes:\n");
-        printf("\t-c\tComprime\n");
-        printf("\t-x\tDescomprime\n");
-        printf("\nExemplo: ./main -c comprima.isso nisso.hx\n");
+        printf("Erro uso inadequado do programa.\n\n");
+        //if (strcmp("", argv[1]) != 0)
+            //printf("Comando %s nao encontrado.", argv[1]);
 
+        instrucoes();
+        
         return 0;
     }
 
     if (strcmp("-c", argv[1]) == 0)
     {
-        if (strstr(argv[3], ".hx"))
+        if (strstr(argv[3], ".pd"))
         {
             CompressFile(argv[2], argv[3]);
         }
         else
         {
-            printf("O arquivo resultante da compressao deve ter a extensao '.hx'.\n");
-            printf("Exemplo: \n\t./main -c comprima.isso nisso.hx\n");
+            printf("O arquivo resultante da compressao deve ter a extensao '.pd'.\n");
+            printf("Exemplo: \n\t./main -c comprima.isso nisso.pd\n");
 
             return 0;
         }
     }
     else if (strcmp("-x", argv[1]) == 0)
     {
-        if (strstr(argv[2], ".hx"))
+        if (strstr(argv[2], ".pd"))
         {
             DecompressFile(argv[2], argv[3]);
         }
         else
         {
-            printf("O arquivo a ser descomprimido deve ter a extensao '.hx'.\n");
-            printf("Exemplo: \n\t./main -x descomprime.hx nisso.extensao\n");
+            printf("O arquivo a ser descomprimido deve ter a extensao '.pd'.\n");
+            printf("Exemplo: \n\t./main -x descomprime.pd nisso.extensao\n");
 
             return 0;
         }
     }
+    else if (strcmp("-h", argv[1]) == 0 || strcmp("--help", argv[1]) == 0) 
+    {
+        instrucoes();        
+        return 0;
+    }
     else
     {
-        printf("Uso: main [OPCAO] [ARQUIVO] [ARQUIVO]\n\n");
-        printf("Opcoes:\n");
-        printf("\t-c\tComprime\n");
-        printf("\t-x\tDescomprime\n");
-        printf("\nExemplo: ./main -c comprima.isso nisso.hx\n");
-        
+        instrucoes();        
         return 0;
     }
 
